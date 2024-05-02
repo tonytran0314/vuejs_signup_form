ba61e2e57f216d1753591a5c86acf71b3a425091
Gia Huy Tran <tonytran0314@gmail.com>
2024-05-02 19:54:04 -0400

    fix not load resources

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-// import Toast from "vue-toastification"
-// import "vue-toastification/dist/index.css"
+import Toast from "vue-toastification"
+import "vue-toastification/dist/index.css"
 import './assets/config.scss'
-createApp(App).mount('#app')
-// const app = createApp(App)
+const app = createApp(App)
 
-// app.mount('#app')
+app.mount('#app')
 
-// app.use(Toast)
+app.use(Toast)
