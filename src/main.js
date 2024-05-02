6ca8d0c4175095f0a1ffbfa0854d65925adc3dd5
Gia Huy Tran <tonytran0314@gmail.com>
2024-05-02 19:44:23 -0400

    fix

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// import Toast from "vue-toastification"
+// import "vue-toastification/dist/index.css"
 import './assets/config.scss'
+createApp(App).mount('#app')
+// const app = createApp(App)
 
-const app = createApp(App)
+// app.mount('#app')
 
-app.mount('#app')
+// app.use(Toast)
